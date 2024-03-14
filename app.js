5f4a4eb92092854336df908b11fefff93cfd31f2
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-03-14 22:07:30 +0530

    code added

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const favMovie = "3idiots";
 
 let guess = prompt("Guess my fav movie");
 
-while( (guess != favMovie) && (guess != "quit")){
+while(guess != favMovie){
+    if(guess == "quit"){
+        console.log("you quit");
+        break;
+    }
     guess = prompt("wrong Please try again");
 }
 
 if( guess == favMovie){
     console.log("congrats");
+} 
+
+let i =1;
+
+while(i<=6){
+    if( i == 4){
+        break;
+    }
+    console.log(i);
+    i++;
 }
