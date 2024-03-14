dfeabae061b54b36c5e8ecda24280e35b0ac12bc
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-03-14 19:53:52 +0530

    code added

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-let n = prompt("write a number");
-   n = parseInt(n);
+let i =1;
 
-for(let i=n; i<=n*10; i=i+n){
+while(i<=10){
     console.log(i);
-}
-
-for(let i=1; i<=3; i++){
-    for(let j=1; j<=3; j++){
-        console.log(j);
-    }
+    i++;
 }
