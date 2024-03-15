47f1509ffe10c277b8576d7e8d15e277cab9e24a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-03-15 20:02:36 +0530

    push code

--- app1.js
+++ app1.js
@@ -1,6 +1,7 @@
 let student = [ ["ayush", 45], ["khitish", 65], ["jha",56]];
 
 for( let i =0; i<student.length; i++){
+    console.log(`student number #${i}`);
     for(let j=0; j<student[i].length; j++){
         console.log(student[i][j]);
     }
